60ad479fb567243358faee425f3569fb802b078f
Misra Ngonga <misra01@icloud.com>
2020-06-13 23:49:57 +0200

    Initial comment. struggled with constraints

---
+++ Lendor/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Lendor
+//
+//  Created by Misra Ngonga on 2020/06/11.
+//  Copyright © 2020 mvnBits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var oweMEBTN: UIButton!
+    @IBOutlet weak var iOweBTN: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+            print("we're home")
+        
+        
+    }
+    
+
+
+
+}
+
